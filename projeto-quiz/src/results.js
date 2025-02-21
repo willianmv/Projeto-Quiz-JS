9b6f56e8842313a30a331796b517e4ba8c42af15
willianmv <willianvmarques300@gmail.com>
2025-02-21 16:24:15 -0300

    Adicionando botão de remover elemento da lista

--- projeto-quiz/src/results.js
+++ projeto-quiz/src/results.js
@@ -1,3 +1,33 @@
+import { createResult, deleteResult, fetchResults } from "./api";
+import { button, listItem } from "./elements";
+
+//Carrega os resultados da API para uma lista no HTML
+export async function loadResults(ul){
+    ul.innerHTML = '';
+    const results = await fetchResults();
+
+    results.forEach((result) =>{
+        const li = listItem("", {
+          id: `result-${result.id}`,
+          innerHTML: `<h3>${result.name}</h3><p>${result.description}</p>`  
+        });
+
+        //Cria botão para exluir o respectivo elemento
+        const deleteButton = button("Excluir Resultado", {
+            type: "button",
+            className: "",
+            onClick: async () => {
+                await deleteResult(result.id);
+                li.remove();
+            }
+        });
+
+        li.append(deleteButton);
+        ul.append(li);
+    })
+}
+
+//Captura os dados do form e passa para API
 export function addSubmitResultListener(form){
     form.addEventListener('submit', async (event) =>{
         event.preventDefault();
@@ -7,6 +37,9 @@ export function addSubmitResultListener(form){
         const nameField = formData.get('name');
         const descriptionFiled = formData.get('description');
 
-        console.log({nameField, descriptionFiled});
+       await createResult(nameField, descriptionFiled);
+
+       form.reset();
+       loadResults(document.getElementById('resultsList'));
     })
 }
